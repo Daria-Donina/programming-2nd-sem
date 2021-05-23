f0b702fca1c988eda8e73a846467e1b93e5095a1
Daria-Donina <donina.daria@yandex.ru>
2021-05-24 00:56:07 +0300

    completed extract_news()

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -1,3 +1,5 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -6,7 +8,29 @@ def extract_news(parser):
     """ Extract news from a given web page """
     news_list = []
 
-    # PUT YOUR CODE HERE
+    # print(parser)
+    links = parser.findAll('a', 'storylink')
+    subtexts = parser.findAll('td', 'subtext')
+
+    for link, subtext in zip(links, subtexts):
+        # author
+        author = subtext.a.text
+
+        link_comment = subtext.find('a', string=re.compile('comment'))
+        comments = link_comment.text.split('\xa0')[0] if link_comment else '0'
+
+        points = subtext.span.text.split(' ')[0]
+
+        title = link.text
+
+        url = link['href']
+        news_list.append({
+            'author': author,
+            'comments': int(comments),
+            'points': int(points),
+            'title': title,
+            'url': url
+        })
 
     return news_list
 
@@ -30,3 +54,4 @@ def get_news(url, n_pages=1):
         n_pages -= 1
     return news
 
+get_news("https://news.ycombinator.com/newest")
